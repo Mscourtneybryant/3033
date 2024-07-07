a08c177faf5e75addd84e6b26606122081373a97
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2024-07-07 14:49:00 -0500

    final commit

--- src/main/java/org/example/ReadStringFromConsole.java
+++ src/main/java/org/example/ReadStringFromConsole.java
@@ -1,6 +1,8 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 public class ReadStringFromConsole {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
